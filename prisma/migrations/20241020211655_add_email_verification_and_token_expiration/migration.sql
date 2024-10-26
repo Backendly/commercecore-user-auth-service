26ae886afd59efb6c61f98033b57fc2ba36940b1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-26 18:24:49 +0000

    fix: Fix database migration file

--- prisma/migrations/20241020211655_add_email_verification_and_token_expiration/migration.sql
+++ prisma/migrations/20241020211655_add_email_verification_and_token_expiration/migration.sql
@@ -1,5 +1,6 @@
 -- AlterTable
-ALTER TABLE "developers" ADD COLUMN     "email_verification_token" VARCHAR,
+ALTER TABLE "developers" 
+ADD COLUMN     "email_verification_token" VARCHAR,
 ADD COLUMN     "password_reset_token" VARCHAR,
 ADD COLUMN     "reset_token_expires_at" TIMESTAMP(6),
 ADD COLUMN     "token_expires_at" TIMESTAMP(6);
